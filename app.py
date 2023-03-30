dc16238a259cbed5a30bae96daaa02e388f187c9
ShiroyaJay <jayshiroya38@gmail.com>
2023-03-30 14:19:50 +0530

    web app done

--- app.py
+++ app.py
@@ -16,11 +16,27 @@ def home():
 @app.route("/predict_api",methods=["POST"])
 def predict_api():
     data= request.json['data']
+
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)[0]
     return jsonify(output)
+
+@app.route('/predict',methods=["POST"])
+def predict():
+   # data=[float(x) for x in request.form.values()]
+    #new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
+   # output=model.predict(new_data)[0]
+    #return render_template("home.html",prediction_text="predicted house price is: {}".format(output))
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is : {}".format(output))
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
